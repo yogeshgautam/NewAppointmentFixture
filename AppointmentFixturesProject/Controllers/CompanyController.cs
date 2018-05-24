a2d73a13ee5c5e39dcd0028241bc96547e825404
Manish <mshbhattarai@outlook.com>
2018-05-24 22:51:06 +0545

    changes in department

--- AppointmentFixturesProject/Controllers/CompanyController.cs
+++ AppointmentFixturesProject/Controllers/CompanyController.cs
@@ -10,20 +10,14 @@ using System.Web.Security;
 using System.Net;
 using PagedList;
 using PagedList.Mvc;
-//
-
-
 namespace AppointmentFixturesProject.Controllers
 {
-
      [Authorize(Roles = "CompanyMaster")]
     public class CompanyController : Controller
     {
-        //temp code
-        
-
+        BLLAppointmentDetails bllappointmentdetails = new BLLAppointmentDetails();
         BLLDepartment bllDepartment = new BLLDepartment();
-         BLLCompany bllCompany=new BLLCompany();
+        BLLCompany bllCompany=new BLLCompany();
         BLLVIP bllvip = new BLLVIP();
         BLLUser bluser = new BLLUser();
         BLLAvailableTiming blavailable = new BLLAvailableTiming();
@@ -59,47 +53,43 @@ namespace AppointmentFixturesProject.Controllers
              return PartialView("_CompanyNamePartial",companyName);
          }
 
-        // GET: Company
-        public ActionResult Index(string search,int ?page)
-        {
-            List<BODepartment> lst = bllDepartment.GetAllDepartment().Where(u => u.CompanyId == companyId).ToList();
-            if (search != null)
-            {
-                lst = bllDepartment.GetAllDepartment().Where(u => u.CompanyId == companyId && u.Name.StartsWith(search)).ToList();
-            }
-            return View(lst.ToPagedList(page??1,5));
-        }
-
-        public ActionResult CreateDepartment()
+       
+        //DepartmentFunctions
+        public ActionResult Index()
         {
             
             return View();
         }
-
-        [HttpPost]
-        public ActionResult CreateDepartment(BODepartment model)
+        public ActionResult GetPaggedData(int pageNumber = 1, int pageSize = 5)
         {
-            if (ModelState.IsValid)
-            {
-                model.CompanyId = companyId;
-                if (bllDepartment.CreateDepartment(model) == 1)
-                {
-                    ViewBag.Message = "Department Created Successfully";
-                }
-                else
-                {
-                    ViewBag.Message = "There was a problem Creating Department. Please Contact Administrator for Support.";
-                }
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                ViewBag.Message = "Please Enter Correct Information";
-            }
-
-            return View();
+            var listData = bllDepartment.GetAllDepartment().OrderByDescending(u => u.Id).ToList();
+            var pagedData = Pagination.PagedResult(listData, pageNumber, pageSize);
+            return Json(pagedData, JsonRequestBehavior.AllowGet);
         }
+        public JsonResult AddDepartments(BODepartment department)
+        {
+            department.CompanyId = companyId;
+            var ae = bllDepartment.CreateDepartment(department);
+            return Json(ae, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetbyIDDepartments(int id)
+        {
+            var ae = bllDepartment.GetDepartmentById(id);
+            return Json(ae, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult UpdateDepartments(BODepartment department)
+        {
+            var ae = bllDepartment.UpdateDepartment(department);
+            return Json(ae, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult DeleteDepartments(int ID)
+        {
+            var ae = bllDepartment.DeleteDepartment(ID);
+            return Json(ae, JsonRequestBehavior.AllowGet);
+        }
+        //DepartmentFunctionEnds
 
+        //VIPFunction
         public ActionResult VIP(string search,int?page)
         {
             List<BOVIPTable> lst = bllvip.GetAllVIP().Where(u=>u.lstDepartment.CompanyId==companyId).ToList();
@@ -109,7 +99,6 @@ namespace AppointmentFixturesProject.Controllers
             }
             return View(lst.ToPagedList(page??1,10));
         }
-
         public ActionResult CreateVIP()
         {
             var x = bllDepartment.GetAllDepartment().Where(u=>u.CompanyId==companyId).ToList();
@@ -147,27 +136,23 @@ namespace AppointmentFixturesProject.Controllers
             ViewBag.Users = y;
             return View();
         }
-
         public ActionResult DeleteVIP(int id)
         {
             BOVIPTable model = bllvip.GetVIPById(id);
             return View(model);
         }
-
         [HttpPost]
         public ActionResult DeleteVIP(BOVIPTable model)
         {
             bllvip.DeleteVIP(model.Id);
             return RedirectToAction("VIP");
         }
-
         public ActionResult EditVIP(int id)
         {
             BOVIPTable vip = bllvip.GetVIPById(id);
             ViewBag.Department = bllDepartment.GetAllDepartment();
             return View(vip);
         }
-
         [HttpPost]
         public ActionResult EditVIP(BOVIPTable model)
         {
@@ -175,55 +160,20 @@ namespace AppointmentFixturesProject.Controllers
             ViewBag.Department = bllDepartment.GetAllDepartment();
             return RedirectToAction("VIP");
         }
-
         public ActionResult DetailsVIP(int id)
         {
             BOVIPTable vip= bllvip.GetVIPById(id);
             return View(vip);
         }
-
-         //for departments 
-        public ActionResult EditDepartment(int id )
-        {
-            BODepartment model = bllDepartment.GetDepartmentById(id);
-            return View(model);
-        }
-
-         [HttpPost]
-        public ActionResult EditDepartment(BODepartment model)
-        {
-            bllDepartment.UpdateDepartment(model);
-            return RedirectToAction("Index");
-        }
-
-         public ActionResult DeleteDepartment(int id)
-         {
-             BODepartment model = bllDepartment.GetDepartmentById(id);
-             return View(model);
-         }
-
-         [HttpPost]
-         public ActionResult DeleteDepartment(BODepartment model)
-         {
-             bllDepartment.DeleteDepartment(model.Id);
-             return RedirectToAction("Index");
-         }
-
-         public ActionResult DetailsDepartment(int id)
-         {
-             BODepartment model = bllDepartment.GetDepartmentById(id);
-             return View(model);
-         }
-
          public ActionResult ScheduleVip(int id)
          {
              List<BOAvailableTiming> lst = blavailable.GetAvailableTimingByVIP(id);
              return View(lst);
              
          }
+        //VIPFunctionEnds
 
-
-        //prinsha parts
+        //MettingFunction
          [HttpGet]
          public ActionResult CreateMeetingOne()
          {
@@ -259,13 +209,11 @@ namespace AppointmentFixturesProject.Controllers
 
              return View();
          }
-
          public JsonResult List()
          {
              var ae = bllMeetingFirst.GetALLMeeting();
              return Json(ae, JsonRequestBehavior.AllowGet);
          }
-
          public JsonResult UpdateMeetingOne(BOMeetingFirst model)
          {
              var ae = bllMeetingFirst.UpdateMeetingOne(model);
@@ -287,12 +235,19 @@ namespace AppointmentFixturesProject.Controllers
              var ae = bllMeetingFirst.AddMeetingFirst(model);
              return Json(ae, JsonRequestBehavior.AllowGet);
          }
+        //MettingFunctionEnds
 
-       
+        //NotificationFunction
+        [HttpGet]
+        public JsonResult GetNotifications()
+        {
+            return Json(bllappointmentdetails.GetAllAppointment(), JsonRequestBehavior.AllowGet);
+        }
+        //NotificationFunctionEnds
 
-     
 
-      
+
+
 
 
     }
---
+++ AppointmentFixturesProject/Scripts/departmentjs/department.js
@@ -0,0 +1,252 @@
+﻿/// <reference path="jquery-1.9.1.intellisense.js" />
+
+$(document).ready(function () {
+
+    GetPageData(1);
+});
+function GetPageData(pageNum, pageSize) {
+    //After every trigger remove previous data and paging
+    $(".tbody").empty();
+    $("#paged").empty();
+    $.getJSON("/Company/GetPaggedData", { pageNumber: pageNum, pageSize: pageSize }, function (response) {
+        var html = "";
+
+        for (var i = 0; i < response.Data.length; i++) {
+            html = html + '<tr><td>' + response.Data[i].Name + '</td><td>' + response.Data[i].HOD + '</td>';
+            html += '<td>' + response.Data[i].phone + '</td><td>' + response.Data[i].Email + '</td>';
+            html += '<td>' + response.Data[i].Details + '</td>';
+            html += '<td><a href="#" class="btn btn-info" onclick="return getbyID(' + response.Data[i].Id + ')">Edit</a>    <a href="#" class="btn btn-danger" onclick="Delete(' + response.Data[i].Id + ')">Delete</a></td>';
+            html += "</tr>";
+
+        }
+
+        $(".tbody").html(html);
+        PaggingTemplate(response.TotalPages, response.CurrentPage);
+    });
+}
+function PaggingTemplate(totalPage, currentPage) {
+    var template = "";
+    var TotalPages = totalPage;
+    var CurrentPage = currentPage;
+    var PageNumberArray = Array();
+
+
+    var countIncr = 1;
+    for (var i = currentPage; i <= totalPage; i++) {
+        PageNumberArray[0] = currentPage;
+        if (totalPage != currentPage && PageNumberArray[countIncr - 1] != totalPage) {
+            PageNumberArray[countIncr] = i + 1;
+        }
+        countIncr++;
+    };
+    PageNumberArray = PageNumberArray.slice(0, 5);
+    var FirstPage = 1;
+    var LastPage = totalPage;
+    if (totalPage != currentPage) {
+        var ForwardOne = currentPage + 1;
+    }
+    var BackwardOne = 1;
+    if (currentPage > 1) {
+        BackwardOne = currentPage - 1;
+    }
+
+    template = "<p>" + CurrentPage + " of " + TotalPages + " pages</p>"
+    template = template + '<ul class="pager">' +
+        '<li class="previous"><a href="#" onclick="GetPageData(' + FirstPage + ')"><i class="fa fa-fast-backward"></i>&nbsp;First</a></li>' +
+        '<li><select ng-model="pageSize" id="selectedId"><option value="20" selected>20</option><option value="50">50</option><option value="100">100</option><option value="150">150</option></select> </li>' +
+        '<li><a href="#" onclick="GetPageData(' + BackwardOne + ')"><i class="glyphicon glyphicon-backward"></i></a>';
+
+    var numberingLoop = "";
+    for (var i = 0; i < PageNumberArray.length; i++) {
+        numberingLoop = numberingLoop + '<a class="page-number active" onclick="GetPageData(' + PageNumberArray[i] + ')" href="#">' + PageNumberArray[i] + ' &nbsp;&nbsp;</a>'
+    }
+    template = template + numberingLoop + '<a href="#" onclick="GetPageData(' + ForwardOne + ')" ><i class="glyphicon glyphicon-forward"></i></a></li>' +
+        '<li class="next"><a href="#" onclick="GetPageData(' + LastPage + ')">Last&nbsp;<i class="fa fa-fast-forward"></i></a></li></ul>';
+    $("#paged").append(template);
+    $('#selectedId').change(function () {
+        GetPageData(1, $(this).val());
+    });
+}
+function getbyID(MeetID) {
+
+    $('#VIPuser').css('border-color', 'lightgrey');
+    $('#Users').css('border-color', 'lightgrey');
+    $('#StartTime').css('border-color', 'lightgrey');
+    $('#EndTime').css('border-color', 'lightgrey');
+    $('#Date').css('border-color', 'lightgrey');
+    $.ajax({
+        url: "/Company/GetbyIDDepartments/" + MeetID,
+        type: "GET",
+        contentType: "application/json;charset=UTF-8",
+        dataType: "json",
+        success: function (result) {
+
+            debugger;
+            $('#Id').val(result.Id);
+            $('#Name').val(result.Name);
+            $('#phone').val(result.phone);
+            $('#Email').val(result.Email);
+            $('#Details').val(result.Details);
+            $('#HOD').val(result.HOD);
+
+            $('#myModal').modal('show')
+            $('#btnUpdate').show();
+            $('#btnAdd').hide();
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+    return false;
+}
+function Delete(ID) {
+    var ans = confirm("Are you sure you want to delete this Record?");
+    if (ans) {
+        $.ajax({
+            url: "/Company/DeleteDepartments/" + ID,
+            type: "POST",
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            success: function (result) {
+             swal("Good job!", "Deleted Succesfully", "success")
+                GetPageData();
+            },
+            error: function (result) {
+                alert(errormessage.responseText);
+            }
+        });
+    }
+}
+
+function Add() {
+    var res = validate();
+    if (res == false) {
+        return false;
+    }
+    var empObj = {
+        ID: $('#Id').val(),
+        Name: $('#Name').val(),
+        Phone: $('#phone').val(),
+        Email: $('#Email').val(),
+        HOD: $('#HOD').val(),
+        Details: $('#Details').val()
+    };
+    $.ajax({
+        url: "/Company/AddDepartments",
+        data: JSON.stringify(empObj),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            swal({
+                title: "Sweet!",
+                text: "New Department Inserted Succesfully",
+                imageUrl: 'thumbs-up.jpg'
+            });
+            GetPageData();
+            $('#myModal').modal('hide');
+            $('body').removeClass('modal-open');
+            $('.modal-backdrop').remove();
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+function clearTextBox() {
+    $('#Id').val("");
+    $('#Name').val("");
+    $('#phone').val("");
+    $('#Email').val("");
+    $('#Details').val("");
+    $('#HOD').val("");
+    $('#btnUpdate').hide();
+    $('#btnAdd').show();
+    $('#Id').css('border-color', 'lightgrey');
+    $('#phone').css('border-color', 'lightgrey');
+    $('#Email').css('border-color', 'lightgrey');
+    $('#Details').css('border-color', 'lightgrey');
+    $('#HOD').css('border-color', 'lightgrey');
+   
+}
+function Update() {
+    var res = validate();
+    if (res == false) {
+        return false;
+    }
+    var empObj = {
+        Id: $('#Id').val(),
+        Name: $('#Name').val(),
+        phone: $('#phone').val(),
+        Email: $('#Email').val(),
+        Details: $('#Details').val(),
+        HOD: $('#HOD').val(),
+    };
+    $.ajax({
+        url: "/Company/UpdateDepartments",
+        data: JSON.stringify(empObj),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+
+        success: function (result) {
+            swal({
+                title: "Sweet!",
+                text: "Department Updated Sucessfully",
+                imageUrl: 'thumbs-up.jpg'
+            });
+            GetPageData(); debugger;
+            $('#myModal').modal('hide');
+            $('#Id').val("");
+            $('#Name').val("");
+            $('#phone').val("");
+            $('#Email').val("");
+            $('#Details').val("");
+            $('#HOD').val("");
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+function validate() {
+    var isValid = true;
+    if ($('#Name').val().trim() == "") {
+        $('#Name').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#Name').css('border-color', 'lightgrey');
+    }
+    if ($('#phone').val().trim() == "") {
+        $('#phone').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#phone').css('border-color', 'lightgrey');
+    }
+    if ($('#Email').val().trim() == "") {
+        $('#Email').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#Email').css('border-color', 'lightgrey');
+    }
+    if ($('#Details').val().trim() == "") {
+        $('#Details').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#Details').css('border-color', 'lightgrey');
+    }
+    if ($('#HOD').val().trim() == "") {
+        $('#HOD').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#HOD').css('border-color', 'lightgrey');
+    }
+
+    return isValid;
+}
